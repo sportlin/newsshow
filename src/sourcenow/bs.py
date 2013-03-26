1fb9319b63d7e419c0372f62aee4d0c43fb15e23
Sport Lin <sport.lin@gmail.com>
2013-03-26 22:17:41 +0800

    Improve tag match logic
    [] as or, + as and
    
    Improve unmatched identify,
    sometimes a topic can be grouped exactly into MAX_GROYP groups.
    So no unmatched groyp is needed.

--- src/sourcenow/bs.py
+++ src/sourcenow/bs.py
@@ -5,11 +5,19 @@ from commonutil import collectionutil
 
 from . import models
 
+def _isPageMatched(pageTags, tags):
+    matched = False
+    for tag in tags:
+        if collectionutil.fullContains(pageTags, tag.split('+')):
+            matched = True
+            break
+    return matched
+
 def _getPagesByTags(pages, tags, returnMatched=True):
     result = []
     for page in pages:
         pageTags = page['source']['tags']
-        matched = collectionutil.fullContains(pageTags, tags)
+        matched = _isPageMatched(pageTags, tags)
         if (returnMatched and matched) or (not returnMatched and not matched):
             result.append(page)
     return result
@@ -57,8 +65,9 @@ def _getTopicGroups(topic, pages, groups, maxGroups=-1):
         return None
     topicGroups = []
     usedTags = set()
-    if maxGroups > 0:
-        groups = groups[:maxGroups - 1]
+    validCount = 0
+    lastValidGroup = None
+    lastTags = None
     for group in groups:
         groupTags = group.get('tags')
         if not groupTags:
@@ -66,22 +75,41 @@ def _getTopicGroups(topic, pages, groups, maxGroups=-1):
         groupPages = _getPagesByTags(topicPages, groupTags)
         if not groupPages:
             continue
-        usedTags.update(groupTags)
+        validCount += 1
         topicGroup = {}
         topicGroup['slug'] = group.get('slug')
         topicGroup['name'] = group.get('name')
         topicGroup['pages'] = groupPages
-        topicGroups.append(topicGroup)
+        if validCount == maxGroups:
+            lastValidGroup = topicGroup
+            lastTags = groupTags
+            break
+        else:
+            usedTags.update(groupTags)
+            topicGroups.append(topicGroup)
     if usedTags:
-        unmatched = _getPagesByTags(topicPages, list(usedTags),
+        allMatched = False
+        if lastTags:
+            maxTags = set(usedTags)
+            maxTags.update(lastTags)
+            unmatcheds = _getPagesByTags(topicPages, list(maxTags),
                                     returnMatched=False)
-    else:
-        unmatched = pages
-    if unmatched:
+            if not unmatcheds:
+                # If the last group is added, all pages are matched.
+                # Then unmachedgroup is not needed.
+                # And the last group take the place which was reserved for unmatched group.
+                topicGroups.append(lastValidGroup)
+                allMatched = True
+        if not allMatched:
+            unmatcheds = _getPagesByTags(topicPages, list(usedTags),
+                                    returnMatched=False)
+    else:# no group is available, all is seen as unmatched.
+        unmatcheds = topicPages
+    if unmatcheds:
         unknownGroup = {
             'slug': 'unknown',
             'name': '',
-            'pages': topicPages,
+            'pages': unmatcheds,
         }
         topicGroups.append(unknownGroup)
 
