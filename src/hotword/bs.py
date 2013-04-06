44a04f24c8693c3f639ea9f80f0850612b2a9092
Sport Lin <sport.lin@gmail.com>
2013-04-06 22:13:07 +0800

    Improve hot word: merge similar words.

--- src/hotword/bs.py
+++ src/hotword/bs.py
@@ -1,22 +1,17 @@
+# coding=utf-8
+
 import datetime
+import logging
 
 from commonutil import dateutil
 
 from sourcenow import snapi
 from . import models
 
-def calculateTopWords():
+def _getTopWords(content):
     import jieba # May fail to load jieba
-    pages = snapi.getSitePages()
+    pwords = jieba.cut(content, cut_all=False)
     words = []
-    titles = []
-    for page in pages:
-        title = page.get('title')
-        if not title:
-            continue
-        titles.append(title)
-    pwords = jieba.cut('\n'.join(titles), cut_all=False)
-
     for word in pwords:
         if len(word) > 1:
             words.append(word)
@@ -29,15 +24,72 @@ def calculateTopWords():
     for word in words:
         if lastWord != word:
             if lastCount >= _MIN_WORD_COUNT:
-                result.append({'w': lastWord, 'c': lastCount})
+                result.append({'name': lastWord, 'count': lastCount})
             lastWord = word
             lastCount = 0
         lastCount += 1
     if lastCount >= _MIN_WORD_COUNT:
-        result.append({'w': lastWord, 'c': lastCount})
+        result.append({'name': lastWord, 'count': lastCount})
 
-    result.sort(key=lambda item: item.get('c'), reverse=True)
+    result.sort(key=lambda item: item.get('count'), reverse=True)
     return result
+
+def _getWordPages(pages, words):
+    result = {}
+    for word in words:
+        wordTitles = set()
+        for page in pages:
+            title = page.get('title')
+            if not title:
+                continue
+            if word['name'] in title:
+                wordTitles.add(title)
+        word['page'] = len(wordTitles)
+        result[word['name']] = wordTitles
+    return result
+
+def _mergeWord(wordTitles, parentWord, childWord):
+    parentTitles = wordTitles[parentWord['name']]
+    childTitles = wordTitles[childWord['name']]
+    d = childTitles.difference(parentTitles)
+    similar = len(d) * 1.0 / len(childTitles) < 0.6
+    if similar:
+        parentTitles.update(childTitles)
+        parentWord['page'] = len(parentTitles)
+        del wordTitles[childWord['name']]
+    return similar
+
+def _mergeWords(wordPages, words):
+    index = 0
+    size = len(words)
+    while index < size:
+        word = words[index]
+        index2 = index + 1
+        children = []
+        while index2 < size:
+            word2 = words[index2]
+            if _mergeWord(wordPages, word, word2):
+                children.append(word2)
+                del words[index2]
+                size -= 1
+            else:
+                index2 += 1
+        if children:
+            word['children'] = children
+        index += 1
+
+def calculateTopWords():
+    pages = snapi.getSitePages()
+    titles = []
+    for page in pages:
+        title = page.get('title')
+        if not title:
+            continue
+        titles.append(title)
+    words = _getTopWords('\n'.join(titles))
+    wordPages = _getWordPages(pages, words)
+    _mergeWords(wordPages, words)
+    return words
 
 def saveWords(items):
     nnow = datetime.datetime.utcnow()
--- src/hotword/hwapi.py
+++ src/hotword/hwapi.py
@@ -2,17 +2,29 @@ import json
 
 from . import bs
 
+def _getTitle(word):
+    words = [word['name']]
+    if word.get('children'):
+        words.append(word['children'][0]['name'])
+    return ' '.join(words)
+
+def _getKeywords(word):
+    keywords = [word['name']]
+    for item in word.get('children', []):
+        keywords.append(item['name'])
+    return ' '.join(keywords)
+
 def getJsonWords():
     words = bs.getWords()
     cloudWords = []
     for word in words:
         cloudWords.append({
-            'text': word['w'],
-            'weight': word['c'],
+            'text': _getTitle(word),
+            'weight': word['page'],
             'link': {
-                    'href': '/search/' + word['w'],
+                    'href': '/search/' + _getKeywords(word),
                     'target': '_blank',
-                    'title': word['c'],
+                    'title': word['page'],
                 },
             })
     return json.dumps(cloudWords)
